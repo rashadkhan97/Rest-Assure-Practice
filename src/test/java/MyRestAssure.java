fff656694633e2dc5d2b122637a92e1e8ebee6f2
Md. Rashadul Islam <rsrashad99@gmail.com>
2025-10-30 15:07:01 +0600

    Setup, User create and search

--- src/test/java/MyRestAssure.java
+++ src/test/java/MyRestAssure.java
@@ -5,9 +5,22 @@ import io.restassured.response.Response;
 import org.apache.commons.configuration.ConfigurationException;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import static io.restassured.RestAssured.given;
 
 public class MyRestAssure {
+    Properties prop;
+    public MyRestAssure() throws IOException { //created a constructor for token load / write
+        // Need to load / write token first to access
+        prop = new Properties();
+        FileInputStream fs = new FileInputStream("./src/test/resources/config.properties");
+        prop.load(fs);
+    }
+
     @Test
     public void Login() throws ConfigurationException {
         RestAssured.baseURI="https://dmoney.roadtocareer.net";
@@ -24,5 +37,36 @@ public class MyRestAssure {
 
         //value and key save
         Utils.setEnv("token", token);
+    }
+
+
+    @Test
+    //search user
+    public void searchUsers() throws IOException {
+
+        RestAssured.baseURI="https://dmoney.roadtocareer.net";
+        Response res=given().contentType("application/json").header("Authorization", "bearer "+prop.getProperty("token"))
+                .when().get("/user/search/id/98821");
+        System.out.println(res.asString());
+
+    }
+
+    @Test
+    //create user
+    public void createUser(){
+        RestAssured.baseURI="https://dmoney.roadtocareer.net";
+        Response res=given().contentType("application/json")
+                .header("Authorization", "bearer "+prop.getProperty("token"))
+                .header("X-AUTH-SECRET-KEY",prop.getProperty("partnerKey"))
+                .body("{\n" +
+                        "  \"name\": \"b162 user 1\",\n" +
+                        "  \"email\": \"b162user2@gmail.com\",\n" +
+                        "  \"password\": \"1234\",\n" +
+                        "  \"phone_number\": \"01934444444\",\n" +
+                        "  \"nid\": \"123756789\",\n" +
+                        "  \"role\": \"customer\"\n" +
+                        "}")
+                .when().post("/user/create");
+        System.out.println(res.asString());
     }
 }
